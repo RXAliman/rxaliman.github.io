b8b24ffe22e95717b6da671e3e4fa08560a2476c
RXAliman <rovicxavier150@gmail.com>
2025-08-12 22:08:10 +0800

    Added welcome section and socials

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,13 +1,23 @@
 import './index.css'
+import { FaFacebook, FaLinkedin, FaGithub } from "react-icons/fa";
 
 const HomePage = () => (
   <>
     <title>Portfolio - Rovic Aliman</title>
-    <div className='h-screen flex flex-col justify-center items-center gap-2 p-3'>
-      <span className='text-3xl text-center'>Welcome to Home Page!</span>
-      <span className='text-base text-center'>I made this using React + Vite</span>
-      <span className='mt-3 text-xs text-gray-500 text-center'>&copy; Rovic Aliman 2025</span>
-    </div>
+    <main>
+      <section className='h-svh bg-neutral-800 flex items-center max-sm:justify-center'>
+        <div className='flex flex-col max-sm:items-center text-white ml-10 max-sm:ml-0'>
+          <span className='text-6xl max-sm:text-4xl font-medium max-sm:text-center'>Hi, I'm RXAliman</span>
+          <span className='text-2xl max-sm:text-lg font-light max-sm:text-center'>Aspiring Front-end Developer</span>
+          <div className='mt-4 flex flex-col items-start gap-1'>
+            <span className='font-light flex max-sm:justify-center gap-2'><FaFacebook className='text-xl' /><a href="https://facebook.com/rovicxavier" target='_blank'>/rovicxavier</a></span>
+            <span className='font-light flex max-sm:justify-center gap-2'><FaLinkedin className='text-xl' /><a href="https://linkedin.com/in/rovic-xavier-aliman-2887ba312/" target='_blank'>Rovic Xavier Aliman</a></span>
+            <span className='font-light flex max-sm:justify-center gap-2'><FaGithub className='text-xl' /><a href="https://github.com/RXAliman" target='_blank'>RXAliman</a></span>
+          </div>
+        </div>
+      </section>
+      <section className='h-screen'></section>
+    </main>
   </>
 );
 
--- src/NotFound.jsx
+++ src/NotFound.jsx
@@ -7,7 +7,7 @@ const NotFoundPage = () => (
     <div className='h-screen flex flex-col justify-center items-center gap-2 p-3'>
       <img src={catGif} width="100px" draggable='false' />
       <span className='text-base text-center'>Sorry... The page you're trying to access is not available.</span>
-      <span className='mt-3 text-xs text-gray-500 text-center'>&copy; Rovic Aliman 2025</span>
+      <span className='mt-3 text-xs text-gray-500 text-center'>&copy; <a href="/">Rovic Aliman</a> 2025</span>
     </div>
   </>
 )
